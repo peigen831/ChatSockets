b1c16e781df5c493ce9ab14bd9760caeb2bea85d
Sharmaine Lim <SharmaineLim@users.noreply.github.com>
2014-12-12 12:58:42 +0800

    Cleaned imports

--- src/coordinator_version/Subserver.java
+++ src/coordinator_version/Subserver.java
@@ -1,21 +1,10 @@
 package coordinator_version;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Properties;
 
 public class Subserver extends Thread {
 	
