22900849ed250face61305b399ebc488121fc615
Sharmaine Lim <SharmaineLim@users.noreply.github.com>
2014-12-12 13:25:08 +0800

    Prepared Pinger; WIP Requires Monitor to be able to set server as available/unavailable Requires to be called when Coordinator is started

---
+++ src/coordinator_version/coordinator/Pinger.java
@@ -0,0 +1,68 @@
+package coordinator_version.coordinator;
+
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.TimerTask;
+
+public class Pinger extends TimerTask {
+	
+	// TODO change monitor type
+	private Object monitor;
+	private String hostName;
+	private int portNumber;
+	private boolean connectionSuccess;
+	
+	private Socket socket;
+    private PrintWriter outputToServer;
+	
+	public Pinger(Object monitor, String hostName, int portNumber) {
+		this.monitor = monitor;
+		this.hostName = hostName;
+		this.portNumber = portNumber;
+		connectionSuccess = false;
+	}
+	
+	@Override
+	public void run() {
+		connectToServer();
+		setupStreams();
+		outputToServer.println("PING");
+		closeConnection();
+		
+		if (connectionSuccess) {
+			// Ask monitor to set server as available
+		}
+		else {
+			// Ask monitor to set server as unavailable
+		}
+	}
+	
+	private void connectToServer() {
+		try {
+			socket = new Socket(hostName, portNumber);
+			connectionSuccess = true;
+		}catch(Exception e) {
+			connectionSuccess = false;
+			e.printStackTrace();
+		}
+	}
+	
+	private void setupStreams() {
+		try {
+			outputToServer = new PrintWriter(socket.getOutputStream(), true);
+			outputToServer.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void closeConnection() {
+		try {
+			outputToServer.close();
+			socket.close();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
