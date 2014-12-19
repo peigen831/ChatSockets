fe6da12efa041d56c00c1c812da819ba6b129baa
Sharmaine Lim <SharmaineLim@users.noreply.github.com>
2014-12-19 15:25:26 +0800

    Refactored method of finding which files were changed; now hopefully robust and changeable to note conflicts and settle them

--- src/coordinator_version/coordinator/FrontSubserver.java
+++ src/coordinator_version/coordinator/FrontSubserver.java
@@ -2,12 +2,12 @@ package coordinator_version.coordinator;
 
 import java.io.IOException;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Properties;
+import java.util.Set;
 
 import coordinator_version.Monitor;
 import coordinator_version.Subserver;
@@ -22,6 +22,7 @@ public class FrontSubserver extends Subserver{
 	private String clientPropPath = Coordinator.CLIENT_FOLDER;
 	public final String ADDED = "ADDED";
 	public final String DELETED = "DELETED";
+	public final String UPDATED = "UPDATED";
 	
 	
 	public FrontSubserver(Socket socket, Monitor monitor){
@@ -43,171 +44,218 @@ public class FrontSubserver extends Subserver{
 
 	private void getIndex() {
 		
-		String index = null;
-		List<String> listIndexToGet = new ArrayList<>();
-		List<String> listIndexToGive = new ArrayList<>();
-		List<String> listToDestroyServer = new ArrayList<>();
-		List<String> listToDestroyClient = new ArrayList<>();
-		
-		Map<String, Long> mapIndexFromClient = new HashMap<>();
-		
-		//for property file
-		//HashMap<String, String> masterlistAction = new HashMap<>();
-		HashMap<String, String> clientPropertyAction = new HashMap<>();
-		
-		//Get client's file index
-		do {
-			try {
-				index = inputFromClient.readLine();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			if (index != null) {
-				if (index.equals("INDEX_DONE")) {
-					break;
-				}
-				String[] dataFromClient = index.split(":");
-				
-				//indicate client's identity
-				if(dataFromClient[0].equals("NAME"))
-					clientPropPath += dataFromClient[1] + ".properties";
-				
-				//indicate files' identity
-				else
-					mapIndexFromClient.put(dataFromClient[0], Long.parseLong(dataFromClient[1]));
-			}
-		} while (index != null);
-		
+String index = null;
+        
+        // Map<File Name, Action> (Action = ADDED, DELETED, UPDATED)
+        Map<String, String> mapClient = new HashMap<>();
+        Map<String, String> mapServer = new HashMap<>();
+        
+        Set<String> listIndexToGet = new HashSet<String>();
+        Set<String> listIndexToGive = new HashSet<String>();
+        // List<String> listOfConflicts = new HashSet<String>();
+        Set<String> listToDestroyServer = new HashSet<String>();
+        Set<String> listToDestroyClient = new HashSet<String>();
+        
+        // Map<File Name, Last Modified>
+        Map<String, Long> mapNewClientIndex = new HashMap<>();
+        
+        //for property file
+        //HashMap<String, String> masterlistAction = new HashMap<>();
+        HashMap<String, String> clientPropertyAction = new HashMap<>();
+        
+        //Get client's file index
+        do {
+            try {
+                index = inputFromClient.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (index != null) {
+                if (index.equals("INDEX_DONE")) {
+                    break;
+                }
+                String[] dataFromClient = index.split(":");
+                
+                //indicate client's identity
+                if(dataFromClient[0].equals("NAME"))
+                    clientPropPath += dataFromClient[1] + ".properties";
+                
+                //indicate files' identity
+                else
+                    mapNewClientIndex.put(dataFromClient[0], Long.parseLong(dataFromClient[1]));
+            }
+        } while (index != null);
+        
 
-		//load server property
-		Properties masterlistProp = Coordinator.propertiesMonitor.loadProperties(Coordinator.MASTER_LIST);
-		
-		//load client's property
-		Properties clientProp = Coordinator.propertiesMonitor.loadProperties(clientPropPath);
-		
-		// Get master list index and compare to client's file index
-		
-		for(String filename: masterlistProp.stringPropertyNames())
-		{
-			String[] value = masterlistProp.getProperty(filename).split("\\|");
-			//last modify of the file
-        	Long serverLastmodify = Long.valueOf(value[0]);
-			//servers that has the file
-			String[] hasFileServers = new String[value.length-2];
-			
-			for(int i = 0; i < value.length-2; i++)
-				hasFileServers[i] = value[i+2];
-			
-
-			Long time = System.currentTimeMillis();
-			HashMap<String, String> serverAddressportMap;
-			
-			// if the file exists on both client and server
-			if (mapIndexFromClient.containsKey(filename)) {
-				long clientDate = mapIndexFromClient.get(filename);
-				
-				// add to client, if the file on server is newer
-				if (clientDate < serverLastmodify&&!value[1].equals("DELETED")) { 
-					serverAddressportMap = Coordinator.propertiesMonitor.getServerAddressportMap(hasFileServers);
-					String masterAction = generateMasterlistAction(time, ADDED, serverAddressportMap);
-					String addressports = stringAddressport(serverAddressportMap);
-//
-					listIndexToGive.add(filename + "|" + addressports);
-					//masterlistAction.put(filename, masterAction);
-					clientPropertyAction.put(filename, Long.toString(time));
-				}
-				
-				// add to client, if the file on client is newer
-				else if (clientDate > serverLastmodify){
-					
-					serverAddressportMap = Coordinator.propertiesMonitor.getServerToGetMap(hasFileServers);
-					String masterAction = generateMasterlistAction(time, ADDED, serverAddressportMap);
-					String addressports = stringAddressport(serverAddressportMap);
-					
-					listIndexToGet.add(filename + "|" + addressports);
-					//masterlistAction.put(filename, masterAction);
-					clientPropertyAction.put(filename, Long.toString((time)));
-				}
-				
-				// if the file on client is not updated, do nothing
-				
-				mapIndexFromClient.remove(filename);
-			}
-			
-			// if the file only exists on the server
-			else {
-				// delete on server
-				if(clientProp.containsKey(filename)){
-					serverAddressportMap = Coordinator.propertiesMonitor.getServerAddressportMap(hasFileServers);
-					//String masterAction = generateMasterlistAction(time, DELETED, serverAddressportMap);
-					String addressports = stringAddressport(serverAddressportMap);
-					
-					
-					listToDestroyServer.add(filename + "|" + addressports);
-					//masterlistAction.put(filename, masterAction);
-				}
-				
-				// add to client
-				else if (!clientProp.containsKey(filename)&&!value[1].equals("DELETED")) {
-					serverAddressportMap = Coordinator.propertiesMonitor.getServerAddressportMap(hasFileServers);
-					//String masterAction = generateMasterlistAction(time, ADDED, serverAddressportMap);
-					String addressports = stringAddressport(serverAddressportMap);
-
-					listIndexToGive.add(filename + "|" + addressports);
-					//masterlistAction.put(filename, masterAction);
-					clientPropertyAction.put(filename, Long.toString(time));
-				}	
-			}
-		}
-		
-		
-		// check for files that exist only on the client
-		for (Entry<String, Long> entry : mapIndexFromClient.entrySet()) {
-			String filename = entry.getKey();
-			long clientDate = entry.getValue();
-			
-			if(masterlistProp.containsKey(filename)){
-				String[] value = masterlistProp.getProperty(filename).split("\\|");
-	        	Long serverLastmodify = Long.valueOf(value[0]);
-				String status = value[1];
-				String[] hasFileServers = new String[value.length-2];
-				
-				for(int i = 0; i < value.length-2; i++)
-					hasFileServers[i] = value[i+2];
-				
-				// delete on client, if the file previously exist on the server
-				if(status.equals(DELETED) && serverLastmodify >= clientDate){
-					
-					listToDestroyClient.add(filename);
-					//masterlistAction.put(filename, System.currentTimeMillis() + ":DELETED");
-				}
-				
-				// add to server, when clientDate is greater than server's file deleted date
-				else{
-					HashMap<String, String> serverportMap = Coordinator.propertiesMonitor.getServerAddressportMap(hasFileServers);
-					long time = System.currentTimeMillis();
-					String masterAction = generateMasterlistAction(time, ADDED, serverportMap);
-					String addressports = stringAddressport(serverportMap);
-
-					listIndexToGive.add(filename + "|" + addressports);
-					//masterlistAction.put(filename, masterAction);
-					clientPropertyAction.put(filename, Long.toString(time));
-				}
-			}
-			//add to server, when server never encounter this file
-			else{
-				String[] hasFileServers = new String[0];
-				long time = System.currentTimeMillis();
-				
-				HashMap<String, String> serverportMap = Coordinator.propertiesMonitor.getServerToGetMap(hasFileServers);
-				String masterAction = generateMasterlistAction(time, ADDED, serverportMap);
-				String addressports = stringAddressport(serverportMap);
-				
-				listIndexToGet.add(filename + "|" + addressports);
-				//masterlistAction.put(filename, masterAction);
-				clientPropertyAction.put(filename, Long.toString((time)));
-			}
-		}
+        //load server property
+        Properties masterlistProp = Coordinator.propertiesMonitor.loadProperties(Coordinator.MASTER_LIST);
+        
+        //load client's property
+        Properties clientProp = Coordinator.propertiesMonitor.loadProperties(clientPropPath);
+        
+        
+        /** Get client's old file index and compare to client's new file index **/
+        Map<String, Long> mapOldClientIndex = new HashMap<>();
+        
+        // Get client's old file index, check if each file is found in new file index
+        for (String filename : clientProp.stringPropertyNames()) {
+            Long oldLastModified = Long.valueOf(clientProp.getProperty(filename));
+            Long newLastModified = mapNewClientIndex.get(filename);
+            
+            mapOldClientIndex.put(filename, oldLastModified);
+            
+            if (!filename.equals("LAST_SYNC")) {
+            	// if file is in new file index, check if modified
+                if (mapNewClientIndex.containsKey(filename)) {
+                    if (newLastModified > oldLastModified) {
+                        mapClient.put(filename, UPDATED);
+                        listIndexToGet.add(filename);
+                    }
+                }
+                // if file is not in new file index, set action as DELETED
+                else {
+                    mapClient.put(filename, DELETED);
+                    listToDestroyServer.add(filename);
+                }
+            }
+        }
+        
+        // Check if each file from new file index is not found in old file index
+        for (String filename : mapNewClientIndex.keySet()) {
+            // if file is not in old file index, set action as ADDED
+            if (!mapOldClientIndex.containsKey(filename)) {
+                mapClient.put(filename, ADDED);
+                listIndexToGet.add(filename);
+            }
+        }
+        
+        
+        /** Get master list index and compare to client's old file index **/
+        Map<String, String> mapServerIndex = new HashMap<>();
+        
+        // Get master list index and compare to client's old file index
+        for (String filename : masterlistProp.stringPropertyNames()) {
+            String value = masterlistProp.getProperty(filename);
+            mapServerIndex.put(filename, value);
+            String[] propertyValues = value.split("\\|");
+            Long lastModified = Long.valueOf(propertyValues[0]);
+            String status = propertyValues[1];
+            
+            // if file is deleted, delete in client
+            if (status == DELETED) {
+            	mapServer.put(filename, DELETED);
+            	listToDestroyClient.add(filename);
+            }
+            
+            // if file is in the client's old file index, check if modified
+            else if (mapOldClientIndex.containsKey(filename)) {
+                if (lastModified > mapOldClientIndex.get(filename)) {
+                    mapServer.put(filename, UPDATED);
+                    listIndexToGive.add(filename);
+                }
+            }
+            // if file is not in the client's old file index, set action as ADDED
+            else {
+                mapServer.put(filename, ADDED);
+                listIndexToGive.add(filename);
+            }
+        }
+        
+        // Check if each file from the client's old file index is not found in the master list
+        for (String filename : mapOldClientIndex.keySet()) {
+            if (!mapServerIndex.containsKey(filename)) {
+                mapServer.put(filename, DELETED);
+                listToDestroyClient.add(filename);
+            }
+        }
+        
+        
+        /** Compare resulting lists for conflicts **/
+        /** For now, conflicting copies will be overwritten by the server's copy,
+         *  in the future, conflicting copies may create a new file **/
+        
+        for (String filename : mapClient.keySet()) {
+            if (mapServer.containsKey(filename)) {
+                String serverStatus = mapServer.get(filename);
+                String clientStatus = mapClient.get(filename);
+                
+                // if file is UPDATED or ADDED on server
+                if (serverStatus == UPDATED || serverStatus == ADDED) {
+                    // if file is UPDATED or ADDED on client, conflict
+                    if (clientStatus == UPDATED || clientStatus == ADDED) {
+                        // listOfConflicts.add(filename);
+                        listIndexToGive.add(filename);
+                        listIndexToGet.remove(filename);
+                    }
+                    // if file is DELETED on client, overwrite from server
+                    else if (clientStatus == DELETED) {
+                        listIndexToGive.add(filename);
+                        listToDestroyServer.remove(filename);
+                    }
+                }
+                
+                // if file is DELETED on server
+                else if (serverStatus == DELETED) {
+                    // if file is UPDATED or ADDED on client, overwrite from client
+                    if (clientStatus == UPDATED || clientStatus == ADDED) {
+                        listIndexToGet.add(filename);
+                        listToDestroyClient.remove(filename);
+                    }
+                }
+            }
+        }
+        
+        
+        /** Get servers for each file **/
+        Long time = System.currentTimeMillis();
+        Set<String> tempList; 
+        
+        tempList = new HashSet<>(listIndexToGet);
+        listIndexToGet.clear();
+        for (String filename : tempList) {
+            String value = masterlistProp.getProperty(filename);
+            String[] serversWithFile = new String[0];
+            if (value != null) {
+	            String[] propertyValues = masterlistProp.getProperty(filename).split("\\|");
+	            serversWithFile = value.replace(propertyValues[0] + "|" + propertyValues[1] + "|",
+	                    "").split("\\|");
+            }
+            HashMap<String, String> serverAddressportMap = Coordinator.propertiesMonitor.
+            											   getServerToGetMap(serversWithFile);
+            String servers = stringAddressport(serverAddressportMap);
+            
+            listIndexToGet.add(filename + "|" + servers);
+            clientPropertyAction.put(filename, Long.toString((time)));
+        }
+        
+        tempList = new HashSet<>(listIndexToGive);
+        listIndexToGive.clear();
+        for (String filename : tempList) {
+            String value = masterlistProp.getProperty(filename);
+            String[] propertyValues = masterlistProp.getProperty(filename).split("\\|");
+            String[] serversWithFile = value.replace(propertyValues[0] + "|" + propertyValues[1] + "|",
+                    "").split("\\|");
+            HashMap<String, String> serverAddressportMap = Coordinator.propertiesMonitor.
+            											   getServerAddressportMap(serversWithFile);
+            String servers = stringAddressport(serverAddressportMap);
+            
+            listIndexToGive.add(filename + "|" + servers);
+            clientPropertyAction.put(filename, Long.toString((time)));
+        }
+        
+        tempList = new HashSet<>(listToDestroyServer);
+        listToDestroyServer.clear();
+        for (String filename : tempList) {
+            String value = masterlistProp.getProperty(filename);
+            String[] propertyValues = masterlistProp.getProperty(filename).split("\\|");
+            String[] serversWithFile = value.replace(propertyValues[0] + "|" + propertyValues[1] + "|",
+                    "").split("\\|");
+            HashMap<String, String> serverAddressportMap = Coordinator.propertiesMonitor.
+            											   getServerAddressportMap(serversWithFile);
+            String servers = stringAddressport(serverAddressportMap);
+            
+            listToDestroyServer.add(filename + "|" + servers);
+        }
 		
 		StringBuilder sb = new StringBuilder();
 		
